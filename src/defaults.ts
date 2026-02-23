aa0b7d8d1f666ed6a2f98f314d6c353e1fb63a01
Florian Matter <florian.matter@idiotikon.ch>
2026-02-23 14:44:12 +0100

    more global.scss

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,5 +1,28 @@
 export const defaults = {
   VBtn: {
-    class: 'idiotikon-btn'
-  }
+    class: 'idiotikon-btn',
+  },
+  VCard: {
+    margin: 0,
+    padding: 0,
+  },
+  VCardTitle: {
+    margin: 0,
+    padding: 0,
+  },
+  VCardSubtitle: {
+    margin: 0,
+    padding: 0,
+  },
+  VCardText: {
+    margin: 0,
+    padding: 0,
+  },
+  VCardItem: {
+    margin: 0,
+    padding: 0,
+  },
+  VContainer: {
+    padding: 0,
+  },
 }
