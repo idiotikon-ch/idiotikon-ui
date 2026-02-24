42081919effddbdbf1005fbb83c093021dd796f3
Florian Matter <florian.matter@idiotikon.ch>
2026-02-24 08:02:06 +0100

    no style in defaults

--- src/defaults.ts
+++ src/defaults.ts
@@ -2,21 +2,6 @@ export const defaults = {
   VBtn: {
     class: 'idiotikon-btn',
   },
-  VCard: {
-    style: 'margin: 0; padding: 0;',
-  },
-  VCardTitle: {
-    style: 'margin: 0; padding: 0;',
-  },
-  VCardSubtitle: {
-    style: 'margin: 0; padding: 0;',
-  },
-  VCardText: {
-    style: 'margin: 0; padding: 0;',
-  },
-  VCardItem: {
-    style: 'margin: 0; padding: 0;',
-  },
   VContainer: {
     padding: 0,
   },
