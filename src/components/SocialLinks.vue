f38780dff4633fa78f75c8b46cf1e51535f232cf
sabrina <sabrina.braendle@idiotikon.ch>
2026-05-10 19:25:08 +0200

    underline only label

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <div v-for="link in links" :key="link.url" class="mb-2">
-      <a :href="link.url" :target="link.target || '_blank'" rel="noopener noreferrer">
-        <img 
-          v-if="link.imgSrc" 
-          :src="link.imgSrc" 
-          :alt="link.label"
-          style="display: inline; height: 1rem; width: 1rem; margin-right: 10px; margin-left: 4px"
-        />
-        <v-icon v-else-if="link.icon" class="mr-2">{{ link.icon }}</v-icon>
-        {{ link.label }}
+      <a
+        :href="link.url"
+        :target="link.target || '_blank'"
+        rel="noopener noreferrer"
+        class="social-link__anchor"
+      >
+        <span class="social-link__icon">
+          <img
+            v-if="link.imgSrc"
+            :src="link.imgSrc"
+            :alt="link.label"
+            style="display: inline; height: 1rem; width: 1rem; margin-right: 10px; margin-left: 4px"
+          />
+          <v-icon v-else-if="link.icon" class="mr-2">{{ link.icon }}</v-icon>
+        </span>
+        <span class="social-link__label">{{ link.label }}</span>
       </a>
     </div>
   </div>
@@ -32,3 +39,15 @@ export default {
   name: 'SocialLinks'
 }
 </script>
+
+<style scoped>
+/* Whole anchor is clickable but only the label has the underline */
+.social-link__anchor {
+  text-decoration: none;
+}
+
+.social-link__label {
+  text-decoration: underline;
+}
+</style>
+
