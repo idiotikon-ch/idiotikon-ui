adb23073a8755bab0387f62cab1e44cea35d874b
sabrina <sabrina.braendle@idiotikon.ch>
2026-05-20 17:21:14 +0200

    uniform subsection titles; plain address in contact

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <!-- Plain mode: simple paragraphs (for legal pages) -->
+  <div v-if="plain">
+    <p v-if="address" v-html="formatMultiline(address)" />
+    <p>
+      <template v-if="phone">Tel.: {{ phone }}</template>
+      <template v-if="phone && email"><br></template>
+      <template v-if="email">E-Mail: <a :href="`mailto:${email}`">{{ email }}</a></template>
+      <template v-if="(phone || email) && website"><br></template>
+      <template v-if="website">Website: <a :href="website">{{ website }}</a></template>
+    </p>
+  </div>
+
+  <!-- Default mode: with icons -->
+  <div v-else>
     <div v-if="address" class="d-flex align-start mb-2">
       <v-icon class="mr-2 mt-1" style="flex-shrink: 0;">{{ mdiMapMarkerOutline }}</v-icon>
       <span v-html="formatMultiline(address)" />
@@ -22,6 +35,8 @@ defineProps<{
   address?: string
   email?: string
   phone?: string
+  website?: string
+  plain?: boolean
 }>()
 
 function formatMultiline(str?: string) {
